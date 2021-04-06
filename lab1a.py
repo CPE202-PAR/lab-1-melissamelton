ea6c5311f8e1e43f39e7891562d506bb892515e0
melissamelton <81577736+melissamelton@users.noreply.github.com>
2021-04-06 16:10:29 -0700

    Location and location tests

--- lab1a.py
+++ lab1a.py
@@ -2,6 +2,7 @@
 from typing import Optional
 from typing import List
 
+
 # Maybe_List (Optional[List]) is either
 # Python List
 # or
@@ -14,18 +15,44 @@ from typing import List
 
 # Maybe_List -> Maybe_integer
 def max_list_iter(int_list: Optional[List]) -> Optional[int]:
-   """finds the max of a list of numbers and returns the value (not the index)
+    """finds the max of a list of numbers and returns the value (not the index)
    If int_list is empty, returns None. If list is None, raises ValueError"""
-   pass
+    if int_list is None:
+        raise ValueError
+    elif len(int_list) == 0:
+        return None
+    elif len(int_list) == 1:
+        return int_list[0]
+    else:
+       maxVal = int_list[0]
+       for value in int_list:
+            if value > maxVal:
+                maxVal = value
+       return value
 
 # Maybe_List -> Maybe_List
 def reverse_list(int_list: Optional[List]) -> Optional[List]:
    """reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
-   pass
+   if int_list is None:
+      raise ValueError
+   elif len(int_list) == 0:
+      return []
+   else:
+      return [int_list[-1]] + reverse_list(int_list[:-1])
+
 
 # Maybe_List -> None
 def reverse_list_mutate(int_list: Optional[List]) -> None:
    """reverses a list of numbers, modifying the input list, returns None
    If list is None, raises ValueError"""
-   pass
+   modList = []
+   if int_list is None:
+       raise ValueError
+   elif len(int_list) == 0:
+       modList = []
+       return None
+   else:
+       modList = [int_list[-1]] + reverse_list_mutate(int_list[:-1])
+       return None
+
--- lab1a_tests.py
+++ lab1a_tests.py
@@ -15,16 +15,47 @@ class TestLab1(unittest.TestCase):
         with self.assertRaises(ValueError):  # used to check for exception
             max_list_iter(tlist)
 
-    def test_reverse(self) -> None:
+    def test_reverse_01(self) -> None:
         intlist = [1,2,3]
         revlist = reverse_list(intlist)
         self.assertEqual(revlist,[3,2,1])
         self.assertEqual(intlist,[1,2,3])
 
+    def test_reverse_02(self) -> None:
+        intList = []
+        revList = []
+        self.assertEqual(revList, reverse_list(intList))
+
+    def test_reverse_03(self) -> None:
+        intList = [8, 10, 5, 0]
+        revList = [0, 5, 10, 8]
+        revList2 = reverse_list(intList)
+        self.assertTrue(revList == revList2)
+        self.assertEqual(revList, reverse_list(intList))
+
+    def test_reverse_04(self) -> None:
+        intList = [901]
+        revList = [901]
+        self.assertEqual(reverse_list(intList), revList)
+
+    def test_reverse_05(self) -> None:
+        intList = None
+        with self.assertRaises(ValueError):  # used to check for exception
+            reverse_list(intList)
+
     def test_reverse_mutate(self) -> None:
         intlist = [1,2,3]
         reverse_list_mutate(intlist)
         self.assertEqual(intlist,[3,2,1])
+
+    def test_reverse_mutate_1(self) -> None:
+        intlist = [1,2,3]
+        self.assertEqual(reverse_list_mutate(intlist), None)
+
+    def test_reverse_mutate_02(self) -> None:
+        intList = None
+        with self.assertRaises(ValueError):  # used to check for exception
+            reverse_list_mutate(intList)
 
 if __name__ == "__main__":
         unittest.main()
